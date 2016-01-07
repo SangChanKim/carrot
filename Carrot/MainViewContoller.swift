1e51e68256079b666c6eacc37289b0164d84a322
Kevin Kim <Kevin_Kim@MacBook-Pro.local>
2016-01-07 16:52:31 -0500

    finished main scene UI

--- Carrot/MainViewContoller.swift
+++ Carrot/MainViewContoller.swift
@@ -7,10 +7,10 @@
 //
 
 import Foundation
+import Parse
 import CircleProgressView
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var usernameLabel: UILabel!
@@ -20,11 +20,21 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var carrotProgressView: CircleProgressView!
     @IBOutlet weak var megaProgressView: CircleProgressView!
     
+    //let user: PFUser = PFUser()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "didRefresh:", forControlEvents: .ValueChanged)
+        tableView.addSubview(refreshControl)
     }
+    
+    override func viewDidLayoutSubviews() {
+        setUpSegmentedControl()
+    }
+    
+    // MARK: - Table View
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int  {
         return 1
@@ -37,9 +47,50 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("transactionCell", forIndexPath: indexPath) as! TransactionTableViewCell
         
-        
-        
         return cell
     }
+    
+    func didRefresh(refreshControl: UIRefreshControl) {
+        refreshControl.endRefreshing()
+    }
+    
+    // MARK: - Segmented Control
+    
+    private func setUpSegmentedControl() {
+        dateSegment.tintColor = UIColor.orangeColor()
+        
+        let attr = NSDictionary(object: UIFont(name: "HelveticaNeue-Light", size: 10.0)!, forKey: NSFontAttributeName)
+        dateSegment.setTitleTextAttributes(attr as [NSObject : AnyObject], forState: .Normal)
+    }
+    
+    // MARK: - Network
+    
+    func getData() {
+        //let query = PFUser.query()
+        PFCloud.callFunctionInBackground("getPurchasesForAccount", withParameters: ["account_id" : PFUser.currentUser()!["account_id"]]) { (result: AnyObject?, error: NSError?) -> Void in
+            if (error == nil) {
+                print("RESULTS = \(result)")
+            } else {
+                print("error = \(error)")
+            }
+        }
+        
+//        PFCloud.callFunctionInBackground("getPurchasesForAccount", withParameters: ["account_id":PFUser.currentUser()!["account_id"]]) { (result: AnyObject, error: NSError) -> Void in
+//            if (error == nil) {
+//                
+//            } else {
+//                print("purchase network error = \(error)")
+//            }
+//        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
