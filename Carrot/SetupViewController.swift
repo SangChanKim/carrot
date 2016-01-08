5f6ce1672741018c43167b59cded7ea3a973ad02
sangchankim <sck970112@gmail.com>
2016-01-07 20:25:29 -0500

    Added a method to retrieve top three posts

--- Carrot/SetupViewController.swift
+++ Carrot/SetupViewController.swift
@@ -7,15 +7,73 @@
 //
 
 import UIKit
+import Parse
 
 class SetupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Hard coded account and customer id
+        let accountId = "56241a14de4bf40b17112f02"
+        let customerId = "56241a13de4bf40b1711224d"
+        PFUser.currentUser()?.setObject(accountId, forKey: "account_id")
+        PFUser.currentUser()?.setObject(customerId, forKey: "customer_id")
+        PFUser.currentUser()!.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                print("success posting account and customer id")
+                
+                
+                let currentUserID = PFUser.currentUser()?.objectId!
+                print("Objectid: \(currentUserID)")
+                
+                
+                PFCloud.callFunctionInBackground("processPurchases", withParameters: ["object_id" : currentUserID!]) { (returnData: AnyObject?, error: NSError?) -> Void in
+                    if (error == nil) {
+                        if let data = returnData {
+                            print("data = \(data)")
+                            
+                            let results = self.convertStringToDictionary(data as! String)
+                            
+                            
+                            var items : [Item] = []
+                            
+                            for (key,value) in results! {
+                                var newItem = Item()
+                                newItem.itemName = key
+                                newItem.price = value as! Double
+                                items.append(newItem)
+                            }
+                            
+                        }
+                    } else {
+                        print("Totals Error = \(error)")
+                    }
+                }
+                
+                
+                
+                
+                
+            } else {
+                print("failed posting account and customer id")
+            }
+        }
+
+        
 
         // Do any additional setup after loading the view.
     }
-
+    
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.dataUsingEncoding(NSUTF8StringEncoding) {
+            let json = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions()) as? [String:AnyObject]
+            return json!
+        }
+        return nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,4 +90,9 @@ class SetupViewController: UIViewController {
     }
     */
 
+}
+
+struct Item {
+    var itemName = ""
+    var price: Double = 0.0
 }
